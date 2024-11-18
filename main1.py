43b3b4cf0f8889134740f6e02715b4c05713c3a0
Maria-kairos <mk4169317@gmail.com>
2024-11-18 19:25:04 +0300

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,17 @@
+#1st program
+print(9 ** 0.5 *5)
+
+#2nd program
+print(99.9 > 99.8 and 1000 != 1000.1)
+
+#3rd program
+print(2 * 2 +2)
+print(2 * (2 +2))
+print((2 * 2 +2) == (2 * (2 +2)))
+
+#4th program
+a = '123.456'
+a = float(a)
+a = a * 10
+a = str(a)
+print(a[3])
